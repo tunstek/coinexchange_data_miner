d365022fe7790b0cee673d7bdad762ceb7aab162
Keith Tunstead <tunstek@tcd.ie>
2017-11-04 10:55:43 +0100

    Back working again - it seems coinexchange have reverted their security changes

---
+++ send_email_config_example.py
@@ -0,0 +1,10 @@
+
+# *** RENAME this file to send_email.config.py ***
+
+
+# This file contains the credentials for your email client
+# eg:
+#   mail_server_username = *SERVER USERNAME*
+#   mail_server_password = *SERVER PASSWORD*
+#   sender = *DISPLAYED SENDER*
+#   receiver = *DISPLAYED RECEIVER*
